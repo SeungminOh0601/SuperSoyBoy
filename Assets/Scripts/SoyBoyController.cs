1c7eb6e4686b2eaadd8010f00d4d7ef0264e9149
Jay Oh <zxc1353@hanmail.net>
2021-12-01 10:05:44 -0500

    added Player Jump on script

--- Assets/Scripts/SoyBoyController.cs
+++ Assets/Scripts/SoyBoyController.cs
@@ -7,16 +7,29 @@ public class SoyBoyController : MonoBehaviour
 {
     public float speed = 14f;
     public float accel = 6f;
+
+    public bool isJumping;
+    public float jumpSpeed = 8f;
+    public float jumpDurationThresold = 0.25f;
+
     private Vector2 input;
     private SpriteRenderer sr;
     private Rigidbody2D rb;
     private Animator animator;
+
+    private float rayCastLengthCheck = 0.005f;
+    private float width;
+    private float height;
+    private float jumpDuration;
 
     private void Awake()
     {
         sr = GetComponent<SpriteRenderer>();
         animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
+
+        width = GetComponent<Collider2D>().bounds.extents.x + 0.1f;
+        height = GetComponent<Collider2D>().bounds.extents.y + 0.2f;
     }
 
     // Start is called before the first frame update
@@ -40,6 +53,25 @@ public class SoyBoyController : MonoBehaviour
             sr.flipX = true;
         }
 
+        if (input.y >= 1f)
+        {
+            jumpDuration += Time.deltaTime;
+        }
+        else
+        {
+            isJumping = false;
+            jumpDuration = 0f;
+        }
+
+        if (PlayerIsOnGround() && isJumping == false)
+        {
+            if (input.y > 0f)
+            {
+                isJumping = true;
+            }
+        }
+
+        if (jumpDuration > jumpDurationThresold) input.y = 0f;
     }
 
     private void FixedUpdate()
@@ -57,5 +89,25 @@ public class SoyBoyController : MonoBehaviour
 
         rb.AddForce(new Vector2(((input.x * speed) - rb.velocity.x) * acceleration, 0));
         rb.velocity = new Vector2(xVelocity, rb.velocity.y);
+
+        if (isJumping && jumpDuration < jumpDurationThresold)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
+        }
+    }
+
+    public bool PlayerIsOnGround()
+    {
+        bool groundCheck1 = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y - height), -Vector2.up, rayCastLengthCheck);
+        bool groundCheck2 = Physics2D.Raycast(new Vector2(transform.position.x + (width - 0.2f), transform.position.y - height), -Vector2.up, rayCastLengthCheck);
+        bool groundCheck3 = Physics2D.Raycast(new Vector2(transform.position.x - (width - 0.2f), transform.position.y - height), -Vector2.up, rayCastLengthCheck);
+        if (groundCheck1 || groundCheck2 || groundCheck3)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
