4a5d7c78bf40d1df4f5cf38f35374d292ef701b6
Jay Oh <zxc1353@hanmail.net>
2021-12-01 10:57:22 -0500

    added DeathParticle and Hazard Prefab

---
+++ Assets/Scripts/Hazard.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hazard : MonoBehaviour
+{
+    public GameObject playerDeathPrefab;
+    public AudioClip deathClip;
+    public Sprite hitSprite;
+
+    private SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            var audioSource = GetComponent<AudioSource>();
+            if (audioSource != null && deathClip != null)
+            {
+                audioSource.PlayOneShot(deathClip);
+            }
+
+            Instantiate(playerDeathPrefab, collision.contacts[0].point, Quaternion.identity);
+            spriteRenderer.sprite = hitSprite;
+
+            Destroy(collision.gameObject);
+        }
+    }
+}
